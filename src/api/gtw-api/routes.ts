b638dbe75fa11b711fdffacc83bd213f0c518625
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2021-10-05 13:51:19 +0200

    Fix bug in the routes

--- src/api/gtw-api/routes.ts
+++ src/api/gtw-api/routes.ts
@@ -17,11 +17,11 @@ GtwRouter
 .get('/counters', addOrigin(Interfaces.GATEWAY), guard(), miscCtrl.getCounters)
 .post('/counters', addOrigin(Interfaces.GATEWAY), guard(), miscCtrl.sendCounters)
 .get('/items/:oid', addOrigin(Interfaces.GATEWAY), guard(), itemsCtrl.neighbourhood)
-.post('/items/register', addOrigin(Interfaces.GATEWAY), guard(), createAudit(Interfaces.GATEWAY, SourceType.ITEM), itemsCtrl.registration)
-.post('/items/remove', addOrigin(Interfaces.GATEWAY), guard(), createAudit(Interfaces.GATEWAY, SourceType.ITEM), itemsCtrl.deleteItems)
-.put('/items/modify', addOrigin(Interfaces.GATEWAY), guard(), createAudit(Interfaces.GATEWAY, SourceType.ITEM), itemsCtrl.updateItem) // Update item
-.delete('/agent/:agid', addOrigin(Interfaces.GATEWAY), guard(), createAudit(Interfaces.GATEWAY, SourceType.NODE), agentCtrl.deleteAgent)
-.get('/agent/:agid/items', addOrigin(Interfaces.GATEWAY), guard(),createAudit(Interfaces.GATEWAY, SourceType.NODE), agentCtrl.getAgentItems) // change to post if depends on update or use query
+.post('/items/register', addOrigin(Interfaces.GATEWAY), guard(), createAudit(SourceType.ITEM), itemsCtrl.registration)
+.post('/items/remove', addOrigin(Interfaces.GATEWAY), guard(), createAudit(SourceType.ITEM), itemsCtrl.deleteItems)
+.put('/items/modify', addOrigin(Interfaces.GATEWAY), guard(), createAudit(SourceType.ITEM), itemsCtrl.updateItem) // Update item
+.delete('/agent/:agid', addOrigin(Interfaces.GATEWAY), guard(), createAudit(SourceType.NODE), agentCtrl.deleteAgent)
+.get('/agent/:agid/items', addOrigin(Interfaces.GATEWAY), guard(),createAudit(SourceType.NODE), agentCtrl.getAgentItems) // change to post if depends on update or use query
 // .post('/items/td', addOrigin(Interfaces.GATEWAY), guard(), agentCtrl.getTd) // Remove once Gateway is updated, deprecated use
 
 // .put('/items/update', addOrigin(Interfaces.GATEWAY), guard(), itemsCtrl.updateItemContent) // Update only TDs non critial properties
