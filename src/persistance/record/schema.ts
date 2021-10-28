234035d905ad0450c735eef72fa6dc871f2ed7c5
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2021-10-28 09:36:35 +0200

    Update: - Add development scripts Bugfix: - Wrong type in record schema, counters repaired

--- src/persistance/record/schema.ts
+++ src/persistance/record/schema.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 import { createRecord, getAggregated } from './statics'
-import { IRecordDocument, IRecordModel, RecordType } from './types'
+import { IRecordDocument, IRecordModel, MessageType } from './types'
 
 const Schema = mongoose.Schema
 
-const RecordTypes = Object.values(RecordType)
+const MessageTypes = Object.values(MessageType)
 
 const RecordSchema = new Schema<IRecordDocument, IRecordModel>({
     messageStatus: { type: String, required: true },
@@ -14,10 +14,10 @@ const RecordSchema = new Schema<IRecordDocument, IRecordModel>({
     timestamp: { type: Number, required: true },
     reqInitiator: { type: Boolean, required: true },
     messageSize: { type: Number, required: true },
-    messageType: { type: String, required: true },
+    messageType: { type: String, required: true, enum: MessageTypes },
     agid: { type: String, required: true },
     cid: { type: String, required: true },
-    requestType: { type: String, required: true, enum: RecordTypes },
+    requestType: { type: String, required: true },
     isProcessed: { type: Boolean, default: false }
 })
 
